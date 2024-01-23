1383de6b128452f4ed3eb35c6f02febfed9b3cd3
MwanikiA <abbymwaniki18@gmail.com>
2024-01-23 12:27:17 +0300

    day2.js

---
+++ WEEK1/day2.js
@@ -0,0 +1,49 @@
+//let school = 'Zindua'
+//if(school === 'Zindua' ){
+//   console.log('Zindua School')
+//} else {
+//    console.log('Not Zindua School')
+//}
+
+/**
+ * @logicaloperators
+ * @strictlyequal  ===
+ * @and &&
+ * @or  //
+ * @greaterthan  >
+ * @lessthan  <
+ * @greaterthanorequato  >=
+ * @lessthanorequalto   <=
+ * @not   !
+ * @notequal   !==
+ * 
+ */
+
+
+//let age =30
+//if ( age >18 ){
+//    console.log('You are an adult')
+//}else{
+//    console.log('You are a minor')
+//}
+
+functions
+ function consoleHello(){
+     console.log('Hello')
+ }
+ consoleHello()
+
+// let name = 'Abby'
+// switch (name){
+//     case 'Abby':
+//         console.log('I am Abby')
+//         break;
+//     case 'Yvonne':
+//         console.log('I am Yvonne')
+//         break;
+//     default:
+//         console.log('I am not Abby or Yvonne')
+
+// }
+
+
